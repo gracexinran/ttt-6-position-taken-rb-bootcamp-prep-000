2cc6ed0437f635d7aa59f4590bb43a8e1eff423e
Xinran Che <gracexinran@gmail.com>
2019-05-03 21:17:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,18 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
+=======
+  if board[index-1] == " "
+    false
+  elsif board[index-1] == ""
+    false
+  elsif board[index-1] == nil
+    false
+  else
+>>>>>>> 10702db99b1cfb6fb0a463484f1c7e7567bc4c35
     true
   end
 end
